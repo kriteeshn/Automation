b1b6897cc2b59e52d88c0e088db006b13555eae6
kriteeshn <155950044+kriteeshn@users.noreply.github.com>
2025-03-20 00:07:52 +0530

    updated semicolon

--- tests/dates.spec.js
+++ tests/dates.spec.js
@@ -4,7 +4,7 @@ test('Date',async()=>{
  const browser = await chromium.launch();
  const context1 = await browser.newcontext();
  const page1 = await context1.newpage();
- await page1.goto('https://testautomationpractice.blogspot.com/');
+ await page1.goto('https://testautomationpractice.blogspot.com/')
 
 
 
